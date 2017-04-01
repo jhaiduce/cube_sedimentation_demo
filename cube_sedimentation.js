30abcdda8dfa9ce0e2f55d7a2f7e2630fe381997
John Haiducek <jhaiduce@picard.local>
2017-04-01 16:52:26 -0400

    Initial commit

---
+++ cube_sedimentation.js
@@ -0,0 +1,100 @@
+"use strict"
+
+// register the application module
+b4w.register("cube_sedimentation_app", function(exports, require) {
+
+// import modules used by the app
+var m_app       = require("app");
+var m_cfg       = require("config");
+var m_data      = require("data");
+var m_preloader = require("preloader");
+var m_ver       = require("version");
+
+// detect application mode
+var DEBUG = (m_ver.type() == "RELEASE");
+
+// automatically detect assets path
+var APP_ASSETS_PATH = m_cfg.get_assets_path("cube_sedimentation");
+
+/**
+ * export the method to initialize the app (called at the bottom of this file)
+ */
+exports.init = function() {
+    m_app.init({
+        canvas_container_id: "main_canvas_container",
+        callback: init_cb,
+        show_fps: false,
+        console_verbose: DEBUG,
+        autoresize: true
+    });
+}
+
+/**
+ * callback executed when the app is initialized 
+ */
+function init_cb(canvas_elem, success) {
+
+    if (!success) {
+        console.log("b4w init failure");
+        return;
+    }
+
+    m_preloader.create_preloader();
+
+    // ignore right-click on the canvas element
+    canvas_elem.oncontextmenu = function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    };
+
+    load();
+}
+
+/**
+ * load the scene data
+ */
+function load() {
+    m_data.load(APP_ASSETS_PATH + "read_data_surfaces_hires_rigid_blend4web.json", load_cb, preloader_cb);
+}
+
+/**
+ * update the app's preloader
+ */
+function preloader_cb(percentage) {
+    m_preloader.update_preloader(percentage);
+}
+
+/**
+ * callback executed when the scene data is loaded
+ */
+function load_cb(data_id, success) {
+
+    if (!success) {
+        console.log("b4w load failure");
+        return;
+    }
+
+    m_app.enable_camera_controls();
+
+    // place your code here
+
+    info_button=document.getElementById('info_button')
+    info_button.style.display='block'
+    info_button.onclick = function() {
+	info_container=document.getElementById('info_container')
+	if(info_container.style.display!='block'){
+	    info_container.style.display = 'block';
+	}
+	else
+	{
+	    info_container.style.display = 'none';
+	}
+    };
+}
+
+
+});
+
+// import the app module and start the app by calling the init method
+b4w.require("cube_sedimentation_app").init();
